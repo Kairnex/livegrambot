989b128ad10f1468c5997bc61b7f66c29f7b7157
Kairnex <codexkairnex@gmail.com>
2025-05-11 03:17:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
-blocked = "bot was blocked by the user"#message that would be displayed if user has blocked you
-start = "Hello! This is your start message!"#start command
-ban = "you were banned by the admin!"#ban message
-unban = "you were unbanned by the admin."#unban message
-text_message = "Message that would be send if somebody writes any text"#answering_text to user
-banned = "you are blocked"#if admin has blocked a user
+blocked = "ʙᴏᴛ ᴡᴀꜱ ʙʟᴏᴄᴋᴇᴅ ʙʏ ᴛʜᴇ ᴜꜱᴇʀ"#message that would be displayed if user has blocked you
+start = "ʜᴇʟʟᴏ! ᴅᴇᴀʀ ʜᴏᴡ ᴄᴀɴ ɪ ʜᴇʟᴘ ʏᴏᴜ ❣"#start command
+ban = "ʏᴏᴜ ᴡᴇʀᴇ ʙᴀɴɴᴇᴅ ʙʏ ᴛʜᴇ ᴀᴅᴍɪɴ!"#ban message
+unban = "ʏᴏᴜ ᴡᴇʀᴇ ᴜɴʙᴀɴɴᴇᴅ ʙʏ ᴛʜᴇ ᴀᴅᴍɪɴ."#unban message
+text_message = "ᴡᴀɪᴛ ᴅᴇᴀʀ ʏᴏᴜ ᴡɪʟʟ ɢᴇᴛ ʏᴏᴜʀ ʀᴇᴘʟʏ ɪɴ ꜰᴇᴡ ᴍɪɴᴜᴛᴇꜱ😊"#answering_text to user
+banned = "ʏᴏᴜ ᴀʀᴇ ʙʟᴏᴄᴋᴇᴅ"#if admin has blocked a user
 main_id = 6999372290 #id without quotes #can be group id
 TOKEN = "8006165946:AAFoIk1txo28CGOg1ekOrGuEyG-VkIfRj6c"#bot token
 
-ADMIN_ID = 123456789  # Replace with your actual Telegram user ID
+ADMIN_ID = 6999372290  # Replace with your actual Telegram user ID
